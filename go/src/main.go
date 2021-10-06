eb56847f7cd040a784e846b5e614ba2b5841325a
njc <njc@njc-pc.attlocal.net>
2021-10-05 21:33:13 -0500

    Finally fixed config file reading

--- go/src/main.go
+++ go/src/main.go
@@ -1,13 +1,44 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
+	"os"
+
 	"github.com/gin-gonic/gin"
 )
 
+type Configuration struct {
+	Host string `json:"Hostname"`
+	Port string `json:"Port"`
+}
+
 func main() {
+
+	config := decodeConfig()
+	log.Println(config.Host)
 
 	var router = gin.Default()
 	router.GET("/", getVehicles)
 	router.GET("/:id", getVehicleByID)
-	router.Run("localhost:8080")
+	router.Run(config.Host + config.Port)
+}
+
+func decodeConfig() Configuration {
+
+	confFile, err := os.Open("/app/config.json")
+	if err != nil {
+		log.Println(err)
+	}
+	defer confFile.Close()
+
+	decoder := json.NewDecoder(confFile)
+	config := Configuration{}
+	err = decoder.Decode(&config)
+	if err != nil {
+		log.Println(err)
+		panic(err)
+	}
+
+	return config
 }
